d45903820a1d5400c9e3699e3043052fc9170cc1
Viktor Kenyz <vkenyz@lohika.com>
2017-05-07 11:10:35 +0300

    Add ecosystem

---
+++ ecosystem.config.js
@@ -0,0 +1,31 @@
+module.exports = {
+    apps: [
+        {
+            name: "lodb-api",
+            script: "index.js",
+            watch: true,
+            ignore_watch: ["node_modules"],
+            env_production: {
+                NODE_ENV: "dev"
+            }
+        }
+    ],
+
+    /**
+     * Deployment section
+     * http://pm2.keymetrics.io/docs/usage/deployment/
+     */
+    deploy: {
+        dev: {
+            user: "root",
+            host: "82.196.8.165",
+            ref: "origin/master",
+            repo: "git@github.com:Skakruk/lodb-api.git",
+            path: "/opt/lodb/api",
+            "post-deploy": "npm install && pm2 startOrRestart ecosystem.config.js --env dev",
+            env: {
+                NODE_ENV: "dev"
+            }
+        }
+    }
+}
